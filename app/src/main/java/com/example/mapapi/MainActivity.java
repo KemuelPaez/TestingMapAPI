07bf5fc84df81720fb7b12ba68ab88ef7f3ec178
Kemuel <s1920589@usls.edu.ph>
2023-02-25 16:54:32 +0800

    First commit Displaying the Google Map. 25/02/2023

---
+++ app/src/main/java/com/example/mapapi/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mapapi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
