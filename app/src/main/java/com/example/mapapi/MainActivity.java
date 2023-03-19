b6952bb21dd7b6b7f894279fe62291ac5da65617
Kemuel <s1920589@usls.edu.ph>
2023-03-19 13:17:25 +0800

    Test Buttons added (Pan Self, Search) Test Setting Boundaries to Bacolod area only Test CameraUpdateFactory Test Marking on map
    19/03/2023

--- app/src/main/java/com/example/mapapi/MainActivity.java
+++ app/src/main/java/com/example/mapapi/MainActivity.java
@@ -3,20 +3,22 @@ package com.example.mapapi;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
+    public Button panSelfBtn;
 
 
     @Override
@@ -26,12 +28,43 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
         mapFragment.getMapAsync(this);
 
+        Button panSelfBtn = (Button) findViewById(R.id.panSelfCam);
+
+        panSelfBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LatLng bacolod = new LatLng(10.675715, 122.952885);
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(bacolod));
+
+            }
+        });
+
+
 
     }
 
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // init zoom min/max preference
+        googleMap.setMinZoomPreference(14.0f);
+        googleMap.setMaxZoomPreference(31.0f);
+
+        LatLng bacolod = new LatLng(10.675715, 122.952885);
+        // Set bounds on bacolod coordinates
+        LatLngBounds bacolodBounds = new LatLngBounds(
+                new LatLng(10.586913,122.910118), // NE Bounds
+                new LatLng(10.712430,122.962391)); // SE Bounds
+
+        mMap.addMarker(new MarkerOptions().position(bacolod).title("You are Here!"));
+        mMap.setLatLngBoundsForCameraTarget(bacolodBounds);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bacolod));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(bacolod));
 
     }
+
 }
