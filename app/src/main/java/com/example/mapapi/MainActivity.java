a2e7019b174497746dd2be43d3a4eea2a1c780ba
Kemuel <s1920589@usls.edu.ph>
2023-04-01 22:43:39 +0800

    Made some changes . . . added permission granter to check if googleplay is available added mylocation checker if location is enabled added zoom controls and marker options added mylocation and compass commentout search function cause not working LMAO help!
    01/04/2023

--- app/src/main/java/com/example/mapapi/MainActivity.java
+++ app/src/main/java/com/example/mapapi/MainActivity.java
@@ -1,43 +1,31 @@
 package com.example.mapapi;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -45,14 +33,9 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
-
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     public GoogleMap googleMap;
-    public Button panToMarker;
     public ImageView imageSearchBtn;
     public EditText inputLocation;
 
@@ -79,43 +62,45 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         }
 
-        imageSearchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String input = inputLocation.getText().toString();
-                if (input == null){
-                    Toast.makeText(MainActivity.this, "Empty Field", Toast.LENGTH_SHORT).show();
-                }else {
 
-                Geocoder geocoder = new Geocoder(MainActivity.this,Locale.getDefault());
-                    Toast.makeText(MainActivity.this, "Geo work", Toast.LENGTH_SHORT).show();
+        // TODO: 01/04/2023 Geocoding search function still not working. Need to find more tutorials.
 
-                try {
-                    List<Address> listAddress = geocoder.getFromLocationName(String.valueOf(inputLocation), 1);
-
-                    if (listAddress.size() > 0){
-                        LatLng latlng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
-
-                        // Set markers
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.title("Searched Marker");
-                        markerOptions.position(latlng);
-                        googleMap.addMarker(markerOptions);
-
-                        // Set & Animate Camera
-                        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 15);
-                        googleMap.animateCamera(cameraUpdate);
-
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    }
-
-                }
-
-            }
-        });
-
+//        imageSearchBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String input = inputLocation.getText().toString();
+//                if (input == null){
+//                    Toast.makeText(MainActivity.this, "Empty Field", Toast.LENGTH_SHORT).show();
+//                }else {
+//
+//                Geocoder geocoder = new Geocoder(MainActivity.this,Locale.getDefault());
+//                    Toast.makeText(MainActivity.this, "Geo work", Toast.LENGTH_SHORT).show();
+//
+//                try {
+//                    List<Address> listAddress = geocoder.getFromLocationName(String.valueOf(inputLocation), 1);
+//
+//                    if (listAddress.size() > 0){
+//                        LatLng latlng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
+//
+//                        // Set markers
+//                        MarkerOptions markerOptions = new MarkerOptions();
+//                        markerOptions.title("Searched Marker");
+//                        markerOptions.position(latlng);
+//                        googleMap.addMarker(markerOptions);
+//
+//                        // Set & Animate Camera
+//                        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 15);
+//                        googleMap.animateCamera(cameraUpdate);
+//
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    }
+//
+//                }
+//
+//            }
+//        });
 
 
     }
@@ -166,7 +151,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             }
         }).check();
+
+
     }
+
+
 
 
     @Override
@@ -207,5 +196,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
     }
+
 
 }
