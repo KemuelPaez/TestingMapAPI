42ba449b7c6c23ab4c504ce342d51cabbbb2d31e
Kemuel <s1920589@usls.edu.ph>
2023-04-01 22:11:35 +0800

    Made some changes . . . added permission granter to check if googleplay is available added mylocation checker if location is enabled added zoom controls and marker options added mylocation and compass
    01/04/2023

--- app/src/main/java/com/example/mapapi/MainActivity.java
+++ app/src/main/java/com/example/mapapi/MainActivity.java
@@ -2,45 +2,116 @@ package com.example.mapapi;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-    private GoogleMap mMap;
-    public Button panSelfBtn;
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    public GoogleMap googleMap;
+    public Button panToMarker;
+    public ImageView imageSearchBtn;
+    public EditText inputLocation;
+
+    boolean isPermissionGranter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
-        mapFragment.getMapAsync(this);
 
-        Button panSelfBtn = (Button) findViewById(R.id.panSelfCam);
+        imageSearchBtn = findViewById(R.id.imageSearchBtn);
+        inputLocation = findViewById(R.id.inputLocation);
 
+        checkPermission();
 
-        panSelfBtn.setOnClickListener(new View.OnClickListener() {
+        if (isPermissionGranter) {
+            if (checkGooglePlayServices()) {
+                SupportMapFragment supportMapFragment = SupportMapFragment.newInstance();
+                getSupportFragmentManager().beginTransaction().add(R.id.container, supportMapFragment).commit();
+                supportMapFragment.getMapAsync(this);
+
+            } else {
+                Toast.makeText(this, "GooglePlay Services Unavailable", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        imageSearchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String input = inputLocation.getText().toString();
+                if (input == null){
+                    Toast.makeText(MainActivity.this, "Empty Field", Toast.LENGTH_SHORT).show();
+                }else {
 
-                LatLng bacolod = new LatLng(10.675715, 122.952885);
+                Geocoder geocoder = new Geocoder(MainActivity.this,Locale.getDefault());
+                    Toast.makeText(MainActivity.this, "Geo work", Toast.LENGTH_SHORT).show();
 
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(bacolod));
+                try {
+                    List<Address> listAddress = geocoder.getFromLocationName(String.valueOf(inputLocation), 1);
 
+                    if (listAddress.size() > 0){
+                        LatLng latlng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
+
+                        // Set markers
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.title("Searched Marker");
+                        markerOptions.position(latlng);
+                        googleMap.addMarker(markerOptions);
+
+                        // Set & Animate Camera
+                        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 15);
+                        googleMap.animateCamera(cameraUpdate);
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    }
+
+                }
 
             }
         });
@@ -49,31 +120,92 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
+    private boolean checkGooglePlayServices() {
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int result = googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if (result == ConnectionResult.SUCCESS) {
 
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        mMap = googleMap;
-        UiSettings mUiSettings = mMap.getUiSettings();
+            return true;
 
-        // init zoom min/max preference
-        googleMap.setMinZoomPreference(14.0f);
-        googleMap.setMaxZoomPreference(31.0f);
+        } else if (googleApiAvailability.isUserResolvableError(result)) {
+            Dialog dialog = googleApiAvailability.getErrorDialog(this, result, 201, new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialogInterface) {
+                    Toast.makeText(MainActivity.this, "Canceled Dialog", Toast.LENGTH_SHORT).show();
+                }
+            });
+            dialog.show();
+        }
 
-
-        LatLng bacolod = new LatLng(10.675715, 122.952885);
-        // Set bounds on bacolod coordinates
-        LatLngBounds bacolodBounds = new LatLngBounds(
-                new LatLng(10.586913,122.910118), // NE Bounds
-                new LatLng(10.712430,122.962391)); // SE Bounds
-
-        mMap.addMarker(new MarkerOptions().position(bacolod).title("You are Here!").draggable(true));
-        mMap.setLatLngBoundsForCameraTarget(bacolodBounds);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(bacolod));
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(bacolod));
-
-        mUiSettings.setZoomControlsEnabled(true);
-
+        return false;
     }
 
+
+    private void checkPermission() {
+        Dexter.withContext(this).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                isPermissionGranter = true;
+                Toast.makeText(MainActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), "");
+                intent.setData(uri);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+
+            }
+        }).check();
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng latlng = new LatLng(10.675716641291453,122.95286893844606);
+
+        // Set markers
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("Searched Marker");
+        markerOptions.position(latlng);
+        googleMap.addMarker(markerOptions);
+
+        // Set & Animate Camera
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 15);
+        googleMap.animateCamera(cameraUpdate);
+
+
+        // Set UI and Controls on map
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setCompassEnabled(true);
+        googleMap.getUiSettings().setZoomGesturesEnabled(true);
+        googleMap.getUiSettings().setScrollGesturesEnabled(true);
+        googleMap.getUiSettings().setRotateGesturesEnabled(false);
+
+        // Enabling the Current location/pos of user
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+
+    }
 
 }
