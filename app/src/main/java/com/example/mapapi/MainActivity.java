a2326012693a0c73b9ee7742b43922ec7e447fc7
Kemuel <s1920589@usls.edu.ph>
2023-04-02 15:07:41 +0800

    Added some updating location to where the current location of the user is and pinpoint it on the map.
    02/04/2023

--- app/src/main/java/com/example/mapapi/MainActivity.java
+++ app/src/main/java/com/example/mapapi/MainActivity.java
@@ -4,9 +4,11 @@ import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
@@ -14,11 +16,24 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.CurrentLocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,6 +41,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -39,6 +56,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public ImageView imageSearchBtn;
     public EditText inputLocation;
 
+    LocationRequest locRequest;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
     boolean isPermissionGranter;
 
     @Override
@@ -51,19 +71,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         checkPermission();
 
-        if (isPermissionGranter) {
-            if (checkGooglePlayServices()) {
-                SupportMapFragment supportMapFragment = SupportMapFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().add(R.id.container, supportMapFragment).commit();
-                supportMapFragment.getMapAsync(this);
-
-            } else {
-                Toast.makeText(this, "GooglePlay Services Unavailable", Toast.LENGTH_SHORT).show();
+        if (checkGooglePlayServices()) {
+            SupportMapFragment supportMapFragment = SupportMapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, supportMapFragment).commit();
+            supportMapFragment.getMapAsync(this);
+            if (isPermissionGranter) {
+                CheckGps();
             }
+
+        } else {
+            Toast.makeText(this, "GooglePlay Services Unavailable", Toast.LENGTH_SHORT).show();
+
         }
 
 
-        // TODO: 01/04/2023 Geocoding search function still not working. Need to find more tutorials.
+        // TODO: 01/04/2023 Geocoding search function still not working. Need to find more tutorials iban outdated :).
 
 //        imageSearchBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -131,7 +153,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
                 isPermissionGranter = true;
-                Toast.makeText(MainActivity.this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Location Permission Granted!", Toast.LENGTH_SHORT).show();
 
             }
 
@@ -156,12 +178,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
-
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        LatLng latlng = new LatLng(10.675716641291453,122.95286893844606);
+        LatLng latlng = new LatLng(10.675716641291453, 122.95286893844606);
 
         // Set markers
         MarkerOptions markerOptions = new MarkerOptions();
@@ -180,22 +200,86 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         googleMap.getUiSettings().setZoomGesturesEnabled(true);
         googleMap.getUiSettings().setScrollGesturesEnabled(true);
         googleMap.getUiSettings().setRotateGesturesEnabled(false);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
 
         // Enabling the Current location/pos of user
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             return;
+
         }
         googleMap.setMyLocationEnabled(true);
 
+    }
+
+    private void CheckGps() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locRequest).setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> locationSettingsResponseTask = LocationServices.getSettingsClient(getApplicationContext()).checkLocationSettings(builder.build());
+
+        locationSettingsResponseTask.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    // If GPS is already enabled
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    GetCurrentUpdate();
+                    Toast.makeText(MainActivity.this, "GPS currently enabled", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+                    if (e.getStatusCode() == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
+                        ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                        try {
+                            resolvableApiException.startResolutionForResult(MainActivity.this, 101);
+                        } catch (IntentSender.SendIntentException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                    // If settings is unavailable
+                    if (e.getStatusCode() == LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE) {
+                        Toast.makeText(MainActivity.this, "Settings Unavailable", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void GetCurrentUpdate() {
+        locRequest = LocationRequest.create();
+        locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locRequest.setInterval(5000);
+        locRequest.setFastestInterval(3000);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locRequest, new LocationCallback() {
+            // This method will display the Lat and Long of the current location via toast of the user and will
+            // update everytime via loop to where the current loc of the user.
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Toast.makeText(MainActivity.this, "Location: "+locationResult.getLastLocation().getLatitude()+": "+locationResult.getLastLocation().getLongitude(), Toast.LENGTH_SHORT).show();
+            }
+        }, Looper.getMainLooper());
 
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101){
+            // GPS is enabled
+            if (resultCode == RESULT_OK){
+                Toast.makeText(this, "GPS is now enabled!", Toast.LENGTH_SHORT).show();
+            }
+            // If GPS is Canceled
+            if (resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Canceled Connection to GPS", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
